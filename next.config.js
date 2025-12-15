1ae53e3be7b9bc199413786114915d2d6ca2650a
shobi-2004 <vibepanurapanda@gmail.com>
2025-12-16 01:03:58 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  
+  output: 'standalone',
+  webpack: (config) => {
+    // Setting resolve.alias to false tells webpack to ignore a module
+    // https://webpack.js.org/configuration/resolve/#resolvealias
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
+  },
+};
+
+module.exports = nextConfig;
